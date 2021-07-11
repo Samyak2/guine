3ce963de95f6d77968463f36f6a53de572819037
Samyak S Sarnayak <samyak201@gmail.com>
2021-07-11 15:57:43 +0530

    add a basic test
    this runs the quine program and compares the output with the actual
    program

---
+++ quine_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io/ioutil"
+	"os/exec"
+	"testing"
+)
+
+func TestQuine(t *testing.T) {
+	out, err := exec.Command("go", "run", "quine.go").Output()
+	if err != nil {
+		t.Errorf("Error in executing go run: %v", err)
+	}
+
+	actualProg, err := ioutil.ReadFile("quine.go")
+	if err != nil {
+		t.Errorf("Error in reading source file: %v", err)
+	}
+
+	outS := string(out)
+	progS := string(actualProg)
+
+	if len(outS) != len(progS) {
+		t.Errorf("Length mistmatch: Actual program is of length %v while output is of length %v", len(actualProg), len(out))
+	}
+
+	for i := range out {
+		if outS[i] != progS[i] {
+			t.Errorf("Mismatch: at index %d, output is %v while actual program is %v", i, outS[i], progS[i])
+		}
+	}
+}
